31fe47259a26cb1509e70c6cd047c7018e6539f4
Caalvarez2020 <caalvarez@josegalan.edu.co>
2020-08-27 14:45:59 -0500

    Programacion funcional

---
+++ Index.js
@@ -0,0 +1,55 @@
+// Funcion pura
+
+/* función multiplicar (numero) {
+    return numero * 5;
+}
+dejemos resultado = multiplicar (10);
+console.log (resultado); */
+
+// comportamiento de la función
+
+/* sea totalcompra = 0;
+function sumarPrecios (cantidad, valor) {
+    return totalcompra = (totalcompra + (cantidad * valor));
+}
+console.log (sumarPrecios (1, 100));
+console.log (sumarPrecios (1, 100));
+console.log (sumarPrecios (1, 100));
+console.log (sumarPrecios (1, 100));
+console.log (sumarPrecios (1, 100));
+ */
+// función normal
+
+/* función multiplicar (numero) {
+    return numero * numero
+}
+console.log (multiplicar (5)); */
+
+// funcion composición funcional
+
+/* función multiplicar (numero) {
+    return numero * numero
+}
+function suma (num1, num2) {
+    return multiplicar (num1) + multiplicar (num2);
+}
+console.log (suma (4,5)) */
+
+// funciones recursivas
+
+/* función saludar () {
+    console.log ('Hola mundo, hola chicos');
+    saludar ();
+}
+saludar (); */
+
+// arreglo o iniciar elementos de cada arreglo
+
+/*const  listaNumeros  =  [ 2 , 4 , 6 , 8 , 10 ] ;
+const  nuevaLista  =  [ ] ;
+ for ( numero  en  listaNumeros )  {
+    consola . log ( listaNumeros [ numero ] )
+    nuevaLista . empujar ( listaNumeros [ numero ] * 2 )
+ }
+ 
+ consola . log ( nuevaLista)*/
